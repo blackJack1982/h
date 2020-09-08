a5618faffdd36d8138097fc5e880a7ee3ca7e3a9
yauh-ask <66479302+yauh-ask@users.noreply.github.com>
2020-09-09 00:16:46 +0200

    Create web_reconnaissance.py

---
+++ web_reconnaissance.py
@@ -0,0 +1,3 @@
+#Passive gathering of info; resources: builtwith.com, netcraft.com, Recon-ng, map, SpiderFoot
+# Info to plan attacks (which tools are used, eg Apache etc.) => catalog of information (selling, exchanging of data)
+
